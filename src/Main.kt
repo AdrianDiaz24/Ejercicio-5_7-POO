4144f537cd8b3ce8bf3522e2d410e9e67169e5a8
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2025-02-22 02:35:08 +0100

    1.0

--- src/Main.kt
+++ src/Main.kt
@@ -2,4 +2,10 @@
 
 fun main() {
 
+    val motocicleta1 = Motocicleta("A", "B", 123, 23)
+    val automovil1 = Automovil("C", "D", 123, TipoAutomovil.DEPORTIVO)
+
+    motocicleta1.mostrarInfo()
+    automovil1.mostrarInfo()
+
 }
